b37b8bf23ef0a563efd7ccc9a2a2832d20fa6b88
MD.AL MAMUN <mamunofficialmail@gmail.com>
2025-04-13 15:43:01 +0600

    ...

--- src/components/InputForm/UsersSummery.jsx
+++ src/components/InputForm/UsersSummery.jsx
@@ -22,7 +22,7 @@ export default function UsersSummery() {
     <div className="max-w-4xl mx-auto px-4 py-8 h-full min-h-screen animate-fade-in">
       <FormTitle title="User Details" />
 
-      <div className=" w-full max-w-2xl mx-auto p-4  rounded-lg dark:bg-gray-800 animate-pop-in">
+      <div className=" w-full max-w-2xl mx-auto p-4  rounded-lg  animate-pop-in">
         <div className="space-y-2 grid grid-cols-2 gap-4">
           <p className="user-detail-item col-span-full">Name: {fullName}</p>
           <p className="user-detail-item col-span-full">Email: {email}</p>
